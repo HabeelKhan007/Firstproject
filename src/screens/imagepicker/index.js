bdc620d5500b6472005192ee2024d0114d59de42
HabeelKhan007 <142988421+HabeelKhan007@users.noreply.github.com>
2023-09-03 22:35:36 -0700

    add multiple images one time

--- src/screens/imagepicker/index.js
+++ src/screens/imagepicker/index.js
@@ -1,7 +1,7 @@
-import { View, Text, Image, Button, PermissionsAndroid, ScrollView } from 'react-native';
+import { View, Text, Image, Button, PermissionsAndroid, ScrollView, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
-import { responsiveScreenWidth,responsiveScreenHeight } from 'react-native-responsive-dimensions';
-import { launchImageLibrary,launchCamera} from 'react-native-image-picker';
+import { responsiveScreenWidth, responsiveScreenHeight, responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
+import { launchImageLibrary, launchCamera } from 'react-native-image-picker';
 import ImagePicker from 'react-native-image-crop-picker';
 import styles from './style';
 import { FlatList } from 'react-native';
@@ -10,19 +10,39 @@ import { FlatList } from 'react-native';
 const TestScreen = () => {
 
     const [images, setImages] = useState([]);
-    
+
+
 
     const openGallery = async () => {
-        ImagePicker.openPicker({
-            cropping:true,
-            multiple:true
-        }).then(newimage => {
-            const multipleImages = [...images,newimage]
-            setImages(multipleImages)
-            console.log("updated array",multipleImages)
-            // setPhoto(image.path)
-        })
+        // if (images.length < 6) {
+
+            console.log('image lenght', images.length)
+            ImagePicker.openPicker({
+                multiple: true,
+            }).then(newimage => {
+                // console.log("updated array", newimage)
+                setImages(newimage)
+                // const multipleImages = [...images, newimage]
+                newimage.forEach(element => {
+                // console.log('selected images',element)
+                // const multipleImages = [...images, element]
+                // console.log(' images',multipleImages)
+             
+                
+                    // setImages(...images,element)
+                });
+                // setImages(multipleImages)
+                // setPhoto(image.path)
+            })
+
+        // } else {
+        //     alert('Limit Exceeded", "You can only add up to 6 images')
+        // }
     }
+
+    // const checkArray = () =>{
+    //     console
+    // }
 
     // const openCamera = async () =>{
     //     const cameraImage = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
@@ -33,25 +53,51 @@ const TestScreen = () => {
     //         setPhoto(result.assets[0].uri)
     //     }
     // }
+    // const deleteImage = (index) => {
+    //     const updatedImages = [...images];
+    //     updatedImages.splice(index, 1);
+    //     setImages(updatedImages);
+    // };
 
-    
+
 
     return (
         <View style={styles.mainview} >
-            <View style={{height:"60%",padding:"10%",width:"90%",borderWidth:1,backgroundColor:"red"}}>   
-            {images.map((image, index)=>{
-                console.log("images", images);
-                return(
-                <Image key={index} source={{ uri: image[0].path }} style={styles.image} />
-                )
-               })
-               }
-            </View>
+
+            <ScrollView horizontal={true} style={styles.scrollview}>
+                <View style={styles.imagesmainview}>
+                    {images.length  !== 0 ?
+                        (
+                            images.map((image, index) =>{
+                                console.log(image)
+                                return(
+                                <View style={styles.imagesview}>
+                                    <Image key={index} source={{ uri: image.path }} style={styles.image} />
+                                    <TouchableOpacity style={styles.touchableopacity} onPress={() => deleteImage(index)}>
+                                        <Text style={styles.touchabletext}>Delete Image</Text>
+                                    </TouchableOpacity>
+                                </View>
+                                )
+                                }
+                            )
+
+                        ) : (
+                            <View>
+                                <Text style={styles.text}>Add Image</Text>
+                            </View>
+                        )
+
+                    }
+
+
+                </View>
+
+            </ScrollView>
             <View style={styles.buttonview}>
                 <Button title='open Gallery' onPress={openGallery} />
             </View>
             {/* <View style={styles.buttonview}>
-                <Button title='Take Image' onPress={openCamera} />
+                <Button title='Take Image' onPress={()=>console.log('cjksabkjb',images)} />
             </View> */}
         </View>
     );
--- src/screens/imagepicker/style.js
+++ src/screens/imagepicker/style.js
@@ -4,21 +4,54 @@ import React from 'react';
 import FontFamily from '../../assets/customfonts/fonts';
 
 const styles = StyleSheet.create({
-  mainview:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center",
+  mainview: {
+    flex: 1,
+    // alignItems:"center",
+    // justifyContent:"center",
   },
-  image:{
-    width:"100%",
-    height:"100%",
-    resizeMode:"center",
-    marginTop:"5%",
-    
+  image: {
+    width: "98%",
+    height: "100%",
+    marginLeft: "1%",
+    // resizeMode:"center",
+    // marginTop:"5%",
+
   },
-  buttonview:{
-    width:"60%",
-    marginTop:"5%",
+  buttonview: {
+    width: "60%",
+    marginTop: "10%",
+    marginBottom: "50%",
+    alignSelf: "center",
+  },
+  scrollview: {
+    backgroundColor: "gainsboro",
+    height: "5%",
+  },
+  imagesmainview: {
+    flexDirection: 'row',
+    height: "40%",
+    marginTop: "3%",
+  },
+  imagesview: {
+    height: responsiveHeight(40),
+    width: responsiveWidth(100),
+  },
+  touchableopacity: {
+    width: "40%",
+    height: "20%",
+    backgroundColor: "red",
+    alignSelf: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginTop: "5%",
+  },
+  touchabletext: {
+    fontSize: 20,
+  },
+  text: {
+    color: "black",
+    fontSize:20,
   },
 });
 
